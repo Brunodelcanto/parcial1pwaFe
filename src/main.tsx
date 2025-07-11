1c5f15466c63588be8a9fb3159dae3f0cd8bac4f
Bruno Del Canto <delcantobruno5@gmail.com>
2025-07-11 09:26:14 -0300

    initial configuration

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <h1>Parcial 2 PWA</h1>
+  </StrictMode>,
+)
